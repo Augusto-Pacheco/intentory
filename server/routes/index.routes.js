e892248ed09e5eaeec693b6541da4a2ed4322f16
Augusto Pacheco <augustop407@outlook.com>
2022-10-15 12:39:18 -0600

    server configurado

--- server/routes/index.routes.js
+++ server/routes/index.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { pool } from "../config/db";
+import { pool } from "../config/db.js";
 
 const router = Router();
 
 router.get('/ping', async (req, res) => {
-    const result = await pool.query('SELECT 1 + 1 AS result');
-    console.log(result);
-    res.json("ping");
+    const [rows] = await pool.query('SELECT 1 + 1 AS result');
+    console.log(rows);
+    res.json(rows);
 });
 
 export default router;
